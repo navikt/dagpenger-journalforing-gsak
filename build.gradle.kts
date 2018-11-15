3aea9eb2b8702cc39a40eb30a7d8dd65803b4e0c
Geir André Lund <geir.andre.lund@nav.no>
2018-11-15 10:43:49 +0100

    Update events and streams library

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ val fuelVersion = "1.15.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("no.nav.dagpenger:streams:0.1.10-SNAPSHOT")
-    implementation("no.nav.dagpenger:events:0.1.6-SNAPSHOT")
+    implementation("no.nav.dagpenger:streams:0.2.1-SNAPSHOT")
+    implementation("no.nav.dagpenger:events:0.1.8-SNAPSHOT")
 
     implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
 
