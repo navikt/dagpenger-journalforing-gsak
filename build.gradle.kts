7316984423b88afb430dc05ad5a66e2a4afef9e2
Nico Kuijlen <nico.kuijlen@nav.no>
2018-11-26 12:42:07 +0100

    Use mavenCentral() instead of repo.adeo.no

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,6 @@ plugins {
     id("info.solidsoft.pitest") version "1.3.0"
 }
 
-buildscript {
-    repositories {
-        maven("https://repo.adeo.no/repository/maven-central")
-    }
-}
-
 apply {
     plugin("com.diffplug.gradle.spotless")
     plugin("com.adarshr.test-logger")
@@ -22,6 +16,7 @@ apply {
 
 repositories {
     jcenter()
+    mavenCentral()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("http://packages.confluent.io/maven")
     maven("https://dl.bintray.com/kotlin/ktor")
