7063d1da75a89dc61ed8e0a308446eb40c44956a
Geir André Lund <geir.andre.lund@nav.no>
2018-12-03 10:45:06 +0100

    Generate json for gihub deployments api

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 ./gradlew dockerPush${VERSION} -x check
 
-curl -X POST -i -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data "{ ref: $VERSION, description: 'Deploy from travis', 'required_contexts': []  }"
+jq -n --arg ref $VERSION '{ "ref": $ref, "description": "Deploy from travis", "required_contexts": []  }' >> deploy.json
+
+echo "DEPLOYING TO GITHUB"
+cat deploy.json
+
+curl -X POST -i -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data @deploy.json
