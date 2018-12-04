d11bca1a13833cf41133cc0050b8f947a7210a1f
Geir André Lund <geir.andre.lund@nav.no>
2018-12-04 12:07:57 +0100

    Deployment stage should fail if github deployment fails

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -10,4 +10,16 @@ jq -n --arg ref $VERSION '{ "ref": $ref, "description": "Deploy from travis", "r
 echo "DEPLOYING TO GITHUB"
 cat deploy.json
 
-curl -X POST -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data @deploy.json
+DEPLOYMENT_RESPONSE=$(curl -X POST -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data @deploy.json)
+
+DEPLOYMENT_ID=$(echo DEPLOYMENT_RESPONSE | jq -r '.id')
+
+
+if [ -z "DEPLOYMENT_ID" ];
+then
+   >&2 echo "Unable to obtain deployment ID"
+   >&2 echo "$DEPLOYMENT_RESPONSE"
+   exit 1
+fi
+
+
