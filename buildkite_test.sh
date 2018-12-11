1a209000e7dcfad57b47985b9aaf4cb16eec5d80
Geir André Lund <geir.andre.lund@nav.no>
2018-12-11 13:34:05 +0100

    Test deploy from buildkite

--- buildkite_test.sh
+++ buildkite_test.sh
@@ -6,4 +6,10 @@ git clone https://github.com/navikt/github-apps-support.git
 export PATH=`pwd`/github-apps-support/bin:$PATH
 
 
+kubectl config current-context
+kubectl config use-context preprod-fss
+kubectl apply -f nais.yaml
+
+
+
 echo $BUILDKITE_GITHUB_DEPLOYMENT_ID
