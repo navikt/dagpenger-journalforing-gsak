50bc87e83fae65b15190763890bede701276d0e5
Geir André Lund <geir.andre.lund@nav.no>
2019-05-20 13:04:10 +0200

    Disable deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
-set -e
-
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
-IMAGE_VERSION=$DOCKER_IMG_NAME:$VERSION
-IMAGE_LATEST=$DOCKER_IMG_NAME:latest
-
-docker build . --pull -t $IMAGE_VERSION -t $IMAGE_LATEST
-
-docker push $DOCKER_IMG_NAME
-
-jq -n --arg ref $VERSION '{ "ref": $ref, "description": "Deploy from travis", "required_contexts": [], "payload": "{\"freestyle\": \"payload\"}"  }' >> deploy.json
-
-echo "DEPLOYING TO GITHUB"
-cat deploy.json
-
-DEPLOYMENT_RESPONSE=$(curl -X POST -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data @deploy.json)
-DEPLOYMENT_ID=$(echo ${DEPLOYMENT_RESPONSE} | jq -r '.id')
 
 
-if [ -z "$DEPLOYMENT_ID" ];
-then
-   >&2 echo "Unable to obtain deployment ID"
-   >&2 echo "$DEPLOYMENT_RESPONSE"
-   exit 1
-fi
-
-
->&2 echo "Created depoyment against github deployment API, deployment id $DEPLOYMENT_ID"
-
-
+echo "DEPLOY DISABLED!"
+#set -e
+#
+#echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+#
+#IMAGE_VERSION=$DOCKER_IMG_NAME:$VERSION
+#IMAGE_LATEST=$DOCKER_IMG_NAME:latest
+#
+#docker build . --pull -t $IMAGE_VERSION -t $IMAGE_LATEST
+#
+#docker push $DOCKER_IMG_NAME
+#
+#jq -n --arg ref $VERSION '{ "ref": $ref, "description": "Deploy from travis", "required_contexts": [], "payload": "{\"freestyle\": \"payload\"}"  }' >> deploy.json
+#
+#echo "DEPLOYING TO GITHUB"
+#cat deploy.json
+#
+#DEPLOYMENT_RESPONSE=$(curl -X POST -H "Authorization: token $GH_TOKEN"  https://api.github.com/repos/navikt/dagpenger-journalforing-gsak/deployments --data @deploy.json)
+#DEPLOYMENT_ID=$(echo ${DEPLOYMENT_RESPONSE} | jq -r '.id')
+#
+#
+#if [ -z "$DEPLOYMENT_ID" ];
+#then
+#   >&2 echo "Unable to obtain deployment ID"
+#   >&2 echo "$DEPLOYMENT_RESPONSE"
+#   exit 1
+#fi
+#
+#
+#>&2 echo "Created depoyment against github deployment API, deployment id $DEPLOYMENT_ID"
+#
+#
